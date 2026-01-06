ca901f6e345a12acf4204dd624781c2d5e745481
Jérôme Béhé <jerome.behe@gmail.com>
2026-01-06 22:04:23 +0100

    reload pictures list on SIGHUP

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# photo_frame
+# Photo frame
+
+
+## Script python
+
+Recharger la liste des photos suite à ajout/suppression
+```sh
+sudo pkill -HUP -f diaporama.py
+```
--- diaporama.py
+++ diaporama.py
@@ -2,10 +2,12 @@
 import os
 import time
 import subprocess
-
+import signal
 
 # --- CONFIGURATION ---
 PICS_PATH = "/home/dje/gdrive_pics/"
+DELAY = 5
+PICTURES = []
 
 
 def clear_pic():
@@ -17,16 +19,26 @@ def display_pic(filepath):
     cmd = ["sudo", "fbi", "-d", "/dev/fb0", "-T", "1", "-a", "--noverbose", "-1", filepath]
     subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
+def load_pictures_list():
+    global PICTURES
+    PICTURES = [f for f in os.listdir(PICS_PATH) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+
+def handler_signal(signum, frame):
+    load_pictures_list()
+
 
 def main():
-    pictures = [f for f in os.listdir(PICS_PATH) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+    load_pictures_list()
 
     while True:
-        for pic in pictures:
+        for pic in PICTURES:
             display_pic(os.path.join(PICS_PATH, pic))
-            time.sleep(5)
+            time.sleep(DELAY)
 
 
+
+
+signal.signal(signal.SIGHUP, handler_signal)
 
 if __name__ == "__main__":
     try:
